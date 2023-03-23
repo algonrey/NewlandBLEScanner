813a3af46b7083c78f42389293bb67c4ae9461ff
algonrey <hola@alberto.gr>
2023-03-23 21:47:52 +0100

    Initial commit.

---
+++ NewlandBLEScanner/NewlandBLEScanner.h
@@ -0,0 +1,18 @@
+//
+//  NewlandBLEScanner.h
+//  NewlandBLEScanner
+//
+//  Created by Berto on 23/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NewlandBLEScanner.
+FOUNDATION_EXPORT double NewlandBLEScannerVersionNumber;
+
+//! Project version string for NewlandBLEScanner.
+FOUNDATION_EXPORT const unsigned char NewlandBLEScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewlandBLEScanner/PublicHeader.h>
+
+
